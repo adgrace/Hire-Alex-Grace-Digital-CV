64a6c7f1b6fd09759ba733d487658198ddcc586b
Alex Grace <alex@grace6.plus.com>
2016-10-10 15:59:21 +0100

    Image Optimisation

--- gulpfile.js
+++ gulpfile.js
@@ -188,14 +188,14 @@ gulp.task('html', ['styles', 'scripts'], () => {
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
+    .pipe($.imagemin({
       optimizationLevel: 3,
       progressive: true,
       interlaced: true,
       // don't remove IDs from SVGs, they are often used
       // as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
-    })))
+    }))
     .pipe(gulp.dest('dist/images'));
 });
 
